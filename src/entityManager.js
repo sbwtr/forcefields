c9f4fc6b46c118bf6c22947e53f72c8207b80a81
sbwtr <a.zukowski@outlook.com>
2026-06-25 11:30:10 +0200

    implemented entity manager

--- src/entityManager.js
+++ src/entityManager.js
@@ -2,7 +2,19 @@ export class EntityManager {
   constructor() {
     this.entityMap = {};
   }
-  Add(name, entity) {}
-  Get(name) {}
-  Update(dt, time) {}
+  Add(name, entity) {
+    if (!(name in this.entityMap)) {
+      this.entityMap[name] = entity;
+      entity.NAME = name;
+      entity.MANAGER = this;
+    }
+  }
+  Get(name) {
+    return this.entityMap[name];
+  }
+  Update(dt, time) {
+    for (const entity in this.entityMap) {
+      entity.Update(dt, time);
+    }
+  }
 }
