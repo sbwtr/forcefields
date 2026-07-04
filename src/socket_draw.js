1cf021a33b208de3f26e995cd94da2253af6a5ec
sbwtr <a.zukowski@outlook.com>
2026-07-04 12:00:59 +0200

    add animation to sockets played when scored

--- src/socket_draw.js
+++ src/socket_draw.js
@@ -1,5 +1,6 @@
 import { Component } from "./component.js";
 import { ctx } from "./ctx.js";
+import { LinearSpline } from "./helpers.js";
 
 export class SocketDraw extends Component {
   static classname = "SocketDraw";
@@ -7,48 +8,53 @@ export class SocketDraw extends Component {
   constructor(params) {
     super();
     this.params = { ...params };
-    this.active = true;
+    this.action = false;
+    this.sizespl = undefined;
+    this.life = undefined;
+    this.maxlife = undefined;
   }
   get NAME() {
     return SocketDraw.classname;
   }
   InitComponent() {
-    this.owner.RegisterHandler("socket.draw", (msg) => this.OnSocketDraw(msg));
+    this.sizespl = new LinearSpline((t, a, b) => a + t * (b - a));
+    this.sizespl.AddPoint(0.0, 50.0);
+    this.sizespl.AddPoint(0.25, 55.0);
+    this.sizespl.AddPoint(0.5, 70.0);
+    this.sizespl.AddPoint(0.75, 55);
+    this.sizespl.AddPoint(1.0, 50.0);
+    this.life = 2.0;
+    this.maxlife = 2.0;
+    this.owner.RegisterHandler("particles.spawn", (msg) =>
+      this.OnParticlesSpawn(msg),
+    );
   }
-  OnSocketDraw(msg) {
-    this.active = msg.value;
+  OnParticlesSpawn(msg) {
+    this.life = 2.0;
+    this.maxlife = 2.0;
+    this.action = msg.value;
   }
-
   Update(dt, time) {
-    if (this.active) {
-      ctx.beginPath();
-      ctx.arc(
-        this.params.spos.x,
-        this.params.spos.y,
-        this.#radius / 4,
-        0,
-        Math.PI * 2,
-        false,
-      );
-      ctx.strokeStyle = `rgba(${this.params.color},1)`;
-      ctx.lineWidth = 6;
-      ctx.stroke();
-      ctx.closePath();
-
-      ctx.beginPath();
-      ctx.arc(
-        this.params.spos.x,
-        this.params.spos.y,
-        this.#radius,
-        0,
-        Math.PI * 2,
-        false,
-      );
-      ctx.strokeStyle = `rgba(${this.params.color},1)`;
-      ctx.lineWidth = 6;
-      ctx.stroke();
-      ctx.closePath();
+    if (this.action && this.life > 0) {
+      this.life -= dt * 5;
+      const t = 1 - this.life / this.maxlife;
+      this.#radius = this.sizespl.GetPoint(t);
+    } else {
+      this.action = false;
     }
+    ctx.beginPath();
+    ctx.arc(
+      this.params.spos.x,
+      this.params.spos.y,
+      this.#radius,
+      0,
+      Math.PI * 2,
+      false,
+    );
+    ctx.strokeStyle = `rgba(${this.params.color},1)`;
+    ctx.lineWidth = 6;
+    ctx.stroke();
+    ctx.closePath();
   }
 }
 
